0d2cc80df93bfa463d3b4508746f27d82fd33880
aish <aishnikam31@gmail.com>
2024-02-12 20:22:30 +0530

    key expiration,for cache

--- string.js
+++ string.js
@@ -7,7 +7,13 @@ async function init(){
     const result = await client.get('msg:3');
     console.log('result ->', result);
 
-   
+    //    await client.set('msg:4', 'Hello, World!', 'EX', 10);
+    
+    // we can expire the keys, so that they are automatically deleted after a certain time.
+    // we can use this feature to implement a simple cache.
+    // like cache keys can be expired after a certain time. we dont need to store them forever.
+    // our database is separate from our cache. we can use cache to store the most frequently accessed data.
+
 }
 
 // how to run this file?
