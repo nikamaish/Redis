741d4caae592229b98a8ff0a7902fbf51a0fb5bd
aish <aishnikam31@gmail.com>
2024-02-15 01:15:22 +0530

    fasting nodejs

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const axios = require('axios').default;
+const client = require('./client');
+const { json } = require('body-parser');
+
+app.get('/', async(req, res) => {
+
+    const cachedValue = await client.get('todos');
+    if(cachedValue){
+        console.log('serving from cache');
+        return res.json(JSON.parse(cachedValue));
+    }
+
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    await client.set('todos',json.stringify(data));
+    await client.expire('todos', 30);
+    return res.json(data);
+})
+app.listen(9000);
